1bb6c7238c701b4292d04aec75acb56afe23b521
ariel <ariel@LAPTOP-4RALSVTL>
2018-12-02 10:50:49 +0200

    test

--- OOP_EX2-Ex4/src/GIS/test.java
+++ OOP_EX2-Ex4/src/GIS/test.java
@@ -19,29 +19,7 @@ public static void main(String[] args) {
 		long f=m1.getUTC();
 		System.out.println(f);
 
-		//String S= d1.getRaw("SSID");
-	//	System.out.println(S);
-//		MetaData m1=new MetaData(d1,5);
-//	
-//		long f=m1.getUTC();
-//		System.out.println(f);
-//		String r=d1.getElement("FirstSeen",5);
-//		String d=d1.getElement("SSID",5);
-//		System.out.println(r);
-//		System.out.println(d);
-//		System.out.println(s);
-//	File file=new File("WigleWifi_20171203085618.csv");
-//	String path;
-//
-//		path = file.getAbsolutePath();
-//		System.out.println(path);
-		//
-//		toKml.kmltoLayer(path,"C:\\Users\\ariel\\Desktop\\Ex2");//only in the project 
-	
-	
-//	csvData d1=new csvData("C:\\Users\\ariel\\git\\Ex2-Ex4\\WigleWifi_20171201110209.csv");
-//	String s=d1.getElement("FirstSeen",2);
-//	System.out.println(s);
+
 }
 	
 
